b2c86dbf3f3c6dd63dfb82343b1e106328657bd8
Keith Kester <keith.a.kester.ctr@army.mil>
2024-07-16 13:31:33 -0500

    seed script fixes

--- src/main/resources/db/migration/V202407081420__seed_data.sql
+++ src/main/resources/db/migration/V202407081420__seed_data.sql
@@ -1,29 +1,29 @@
 INSERT INTO CATALOG (CATALOG_ID, NAME, DESCRIPTION, START_DATE, END_DATE, CREATED_DATE)
-VALUES (1, 'sears', 'Sears Roebuck', '2024-01-01', '2025-01-01', current_timestamp);
+VALUES (nextval('catalog_catalog_id_seq'), 'sears', 'Sears Roebuck', '2024-01-01', '2025-01-01', current_timestamp);
 
 INSERT INTO CATALOG (CATALOG_ID, NAME, DESCRIPTION, START_DATE, END_DATE, CREATED_DATE)
-VALUES (2, 'cabellas', 'Cabellas', '2024-01-01', '2025-01-01', current_timestamp);
+VALUES (nextval('catalog_catalog_id_seq'), 'cabellas', 'Cabellas', '2024-01-01', '2025-01-01', current_timestamp);
 
 INSERT INTO CATALOG (CATALOG_ID, NAME, DESCRIPTION, START_DATE, END_DATE, CREATED_DATE)
-VALUES (3, 'jcpenny', 'JC Pennys', '2024-01-01', '2025-01-01', current_timestamp);
+VALUES (nextval('catalog_catalog_id_seq'), 'jcpenny', 'JC Pennys', '2024-01-01', '2025-01-01', current_timestamp);
 
 INSERT INTO CATEGORY (CATEGORY_ID, NAME, DISPLAY_TEXT, CREATED_DATE)
-VALUES (1, 'home', 'Home Goods', current_timestamp);
+VALUES (nextval('category_category_id_seq'), 'home', 'Home Goods', current_timestamp);
 
 INSERT INTO CATEGORY (CATEGORY_ID, NAME, DISPLAY_TEXT, CREATED_DATE)
-VALUES (2, 'sporting', 'Sporting Goods', current_timestamp);
+VALUES (nextval('category_category_id_seq'), 'sporting', 'Sporting Goods', current_timestamp);
 
 INSERT INTO CATEGORY (CATEGORY_ID, NAME, DISPLAY_TEXT, CREATED_DATE)
-VALUES (3, 'electronics', 'Electronics', current_timestamp);
+VALUES (nextval('category_category_id_seq'), 'electronics', 'Electronics', current_timestamp);
 
 INSERT INTO PRODUCT (PRODUCT_ID, NAME, DESCRIPTION, SKU, CREATED_DATE, CATEGORY_ID)
-VALUES (1, 'Coffee Mug', 'This is a description of Coffee Mug', 'abc-123', current_timestamp, 1);
+VALUES (nextval('product_product_id_seq'), 'Coffee Mug', 'This is a description of Coffee Mug', 'abc-123', current_timestamp, 1);
 
 INSERT INTO PRODUCT (PRODUCT_ID, NAME, DESCRIPTION, SKU, CREATED_DATE, CATEGORY_ID)
-VALUES (2, 'Toothpicks', 'This is a description of Toothpicks', 'cba-321', current_timestamp, 2);
+VALUES (nextval('product_product_id_seq'), 'Toothpicks', 'This is a description of Toothpicks', 'cba-321', current_timestamp, 2);
 
 INSERT INTO PRODUCT (PRODUCT_ID, NAME, DESCRIPTION, SKU, CREATED_DATE, CATEGORY_ID)
-VALUES (3, 'Umbrella', 'This is a description of Umbrella', 'acb-132', current_timestamp, 3);
+VALUES (nextval('product_product_id_seq'), 'Umbrella', 'This is a description of Umbrella', 'acb-132', current_timestamp, 3);
 
 INSERT INTO CATALOG_PRODUCT (catalog_id, product_id)
 VALUES (1,1);
